51db82f5813e868e8be0fa5baa22f4d3311d5d29
MuzaffarMohammed <muzaffarmohammed43@gmail.com>
2022-12-22 20:39:16 +0530

    Added mail send

--- functions/api.js
+++ functions/api.js
@@ -1,14 +1,65 @@
 const express = require('express');
 const serverless = require('serverless-http');
+const nodemailer = require('nodemailer');
 
 const app = express();
 const router = express.Router();
-
+const dotenv = require('dotenv');
+dotenv.config();
 
 router.get("/", (req, res) => {
     res.json({
-        message: "Hosting Serveless Function!"
+        message: "This is Appseonit Technologies Private Limited Intellectual Property, any kind of hampering or causing trouble by anyone will be subjected to legal terms!"
     })
+});
+
+router.post('/send', (req, res) => {
+    const output = `
+      <p>You have a new contact request through your website</p>
+      ${req.body.clientId == '1' ? ('Subject:' + req.body.subject) : ''}
+      <h3>Contact Details</h3>
+      <ul> 
+        <li>Name: ${req.body.name}</li>
+        <li>Email: ${req.body.email}</li>
+        <li>Contact Number: ${req.body.contactNo}</li>
+      </ul>
+      <h3>Message</h3>
+      <p>${req.body.message}</p>
+    `;
+
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        host: process.env[`CLIENT_${req.body.clientId}_HOST`],
+        port: process.env[`CLIENT_${req.body.clientId}_PORT`],
+        secure: true, // true for 465, false for other ports
+        auth: {
+            user: process.env[`CLIENT_${req.body.clientId}_USER`],
+            pass: process.env[`CLIENT_${req.body.clientId}_PASS`]
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
+
+    // setup email data with unicode symbols
+    let mailOptions = {
+        from: req.body.name + ' ' + req.body.email, // sender address
+        to: process.env[`CLIENT_${req.body.clientId}_USER`], // list of receivers
+        subject: 'A contact request from your website', // Subject line
+        // text: 'Hello world?', // plain text body
+        html: output // html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+        res.send("OK");
+    });
 });
 
 app.use('/', router);
